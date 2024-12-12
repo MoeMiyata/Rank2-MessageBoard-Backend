7709f759acd4215ae74de3765c5245ab7481c631
MIYATA Moe <moe.miyata.1025@gmail.com>
2024-12-12 13:36:31 +0900

    backend commit (デプロイ)

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -1,21 +1,21 @@
 // import { env } from 'process';
 import { DataSource } from 'typeorm';
 
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 // require('dotenv').config();
 
 const AppDataSource = new DataSource({
   type: 'postgres', // データベースの種別。今回はpostgresqlへの接続とします。
-  // host: 'localhost',
   host: process.env.DB_HOST,
-  // username: 'miyatamoe',
   username: process.env.DB_USER,
-  // password: 'miyatamoe',
   password: process.env.DB_PASS,
-  // database: 'postgres',
   database: process.env.DB_NAME,
+  // host: 'localhost',
+  // username: 'miyatamoe',
+  // password: 'miyatamoe',
+  // database: 'postgres',
   entities: ['src/entities/*.ts'], //  エンティティファイル（後述）配列
   migrations: ['src/migrations/*.ts'], // マイグレーションファイル（後述）配列
 });
