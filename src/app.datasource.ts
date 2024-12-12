02f9eec38d5241f7f0dd76543aa4f95b2823057e
MIYATA Moe <moe.miyata.1025@gmail.com>
2024-12-12 13:39:42 +0900

    backend commit (デプロイ)

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -8,14 +8,19 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres', // データベースの種別。今回はpostgresqlへの接続とします。
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
   // host: 'localhost',
   // username: 'miyatamoe',
   // password: 'miyatamoe',
   // database: 'postgres',
+  // 以下でデプロイ用
+  host: 'dpg-ctd69pm8ii6s738v2np0-a',
+  username: 'twosix',
+  password: 'YnT8gCisClIVUSmVT1UuNjnOWIgxSSPW',
+  database: 'twosix_ko4d',
   entities: ['src/entities/*.ts'], //  エンティティファイル（後述）配列
   migrations: ['src/migrations/*.ts'], // マイグレーションファイル（後述）配列
 });
