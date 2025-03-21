7ba914013cd4c442fac89cf4ac9eecba5e98ab4c
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-03-21 17:22:52 +0900

    passのconsole表示

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -145,9 +145,13 @@ export class UserService {
     if (email !== undefined) {
       updateData.email = email;
     }
-    // if (password !== undefined) {
-    //   updateData.hash = createHash('md5').update(password).digest('hex');
-    // }
+    if (password !== undefined) {
+      // updateData.hash = createHash('md5').update(password).digest('hex');
+      console.log(
+        'new pass',
+        (updateData.hash = createHash('md5').update(password).digest('hex')),
+      );
+    }
     if (birthday !== undefined) {
       updateData.birthday = new Date(birthday);
       // updateData.birthday = birthday;
