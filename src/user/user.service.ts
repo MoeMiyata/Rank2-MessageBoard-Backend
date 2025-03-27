c2967e0acf4a32af88fd5c3be7df68f67b3a1a47
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-03-27 11:11:49 +0900

    passwordの条件分岐をまたはからかつに変更

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -145,7 +145,7 @@ export class UserService {
     if (email !== undefined) {
       updateData.email = email;
     }
-    if (password !== undefined || password !== '') {
+    if (password !== undefined && password !== '') {
       // 空文字の時も登録しない
       // updateData.hash = createHash('md5').update(password).digest('hex');
       console.log('in updateUser(password):', password);
