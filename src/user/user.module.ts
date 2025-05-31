07774587ec2da305c9f3cc52de68e275d27e2a33
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-05-31 18:20:42 +0900

    error/maillink: UserModuleにJwtServiceを注入

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,9 +5,18 @@ import { UserService } from './user.service';
 import { User } from '../entities/user.entity';
 import { Auth } from '../entities/auth.entity';
 
+import { JwtModule } from '@nestjs/jwt'; // ← 追加
+import { MailService } from '../mail/mail.service'; // ← 追加
+
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Auth])],
+  imports: [
+    TypeOrmModule.forFeature([User, Auth]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'Yf9!kPz6#QsL2v@Rm8TnC$1WbXeVz9dA', // .envから秘密鍵を取得
+      signOptions: { expiresIn: '15m' }, // 任意の有効期限を設定
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, MailService],
 })
 export class UserModule {}
