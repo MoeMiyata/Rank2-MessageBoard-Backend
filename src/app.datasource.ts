17f43ce6566d193aab61df29baa71ae8683b6461
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-06-05 10:19:18 +0900

    revise/env: database情報を.env方取得するように変更

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -8,19 +8,19 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres', // データベースの種別。今回はpostgresqlへの接続とします。
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   // host: 'localhost',
   // username: 'miyatamoe',
   // password: 'miyatamoe',
   // database: 'postgres',
-  // 以下デプロイ用
-  host: 'dpg-d0rra215pdvs738u3f6g-a',
-  username: 'two_six',
-  password: 'vn8KjffZFp8RvCfqstALVflQrG8EONt3',
-  database: 'postgresrank2',
+  // // 以下デプロイ用
+  // host: 'dpg-d0rra215pdvs738u3f6g-a',
+  // username: 'two_six',
+  // password: 'vn8KjffZFp8RvCfqstALVflQrG8EONt3',
+  // database: 'postgresrank2',
   entities: ['src/entities/*.ts'], //  エンティティファイル（後述）配列
   migrations: ['src/migrations/*.ts'], // マイグレーションファイル（後述）配列
 });
