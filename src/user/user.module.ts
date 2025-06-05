ef5d5168d499a37d00878903be2368f10d628718
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-06-05 12:23:06 +0900

    error/module: TypeOrmModule.forFeatureにToken追加

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,10 +7,11 @@ import { Auth } from '../entities/auth.entity';
 
 import { JwtModule } from '@nestjs/jwt'; // ← 追加
 import { MailService } from '../mail/mail.service'; // ← 追加
+import { Token } from 'src/entities/token.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Auth]),
+    TypeOrmModule.forFeature([User, Auth, Token]),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'Yf9!kPz6#QsL2v@Rm8TnC$1WbXeVz9dA', // .envから秘密鍵を取得
       signOptions: { expiresIn: '15m' }, // 任意の有効期限を設定
